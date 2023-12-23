8294f8c5a21e566f253c3b098e01bb3ed37af703
Matthew Maloney <matthewgmaloney@gmail.com>
2023-12-22 19:03:45 -0600

    itemCard complete with addToCart but untested

--- utils/ViewDirector.js
+++ utils/ViewDirector.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { useAuth } from './context/authContext';
 import Loading from '../components/Loading';
 import Signin from '../components/Signin';
 import NavBar from '../components/NavBar';
 import RegisterForm from '../components/RegisterForm';
-import { useEffect, useState } from 'react';
 import { hasOrderCheck } from './data/orderDate';
 import OrderContext from './context/orderContext';
 
@@ -25,7 +25,7 @@ const ViewDirectorBasedOnUserAuthStatus = ({ component: Component, pageProps }) 
   if (user) {
     return (
       <>
-        <OrderContext.Provider value={(order, setOrder)}>
+        <OrderContext.Provider value={{ order, setOrder }}>
           <NavBar /> {/* NavBar only visible if user is logged in and is in every view */}
           <div className="container">{'valid' in user ? <RegisterForm user={user} updateUser={updateUser} /> : <Component {...pageProps} />}</div>
         </OrderContext.Provider>
