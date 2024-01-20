88fb6ce7b3108e45c90024ccbfb67a3c2e7e1551
Matthew Maloney <matthewgmaloney@gmail.com>
2024-01-20 09:22:41 -0600

    fix for eventDate not displaying on ViewEvent

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -41,40 +41,42 @@ export default function EventDetails() {
 
   return (
     <>
-      <div className="event-card-container">
-        <Card className="event-card" style={{ width: '600px', height: '800px' }}>
-          <Card.Header>{event.name}</Card.Header>
-          <div className="event-card-detail">
-            <Card.Img variant="top" className="event-card-img" src={event.image_url} style={{ width: '300px' }} />
-            <Card.Body>
-              <Card.Text>{event.description}</Card.Text>
-              <Card.Text>Ticket Price ${event.ticket?.price}</Card.Text>
-              <Card.Text>Tickets Available: {event.tickets_available}</Card.Text>
-              <Card.Text>{format(new Date(eventDate), 'yyyy-M-d, h:mmbbb')}</Card.Text>
-              <Card.Text>
-                Seller:{' '}
-                <Link className="seller-link" passHref href={`/seller/${event.seller?.id}`}>
-                  {`${event.seller?.first_name} ${event.seller?.last_name}`}
-                </Link>
-              </Card.Text>
-              {event.seller?.id !== user.id ? (
-                <Button variant="primary" onClick={handleAddToCart}>
-                  Add ticket to Cart
-                </Button>
-              ) : (
-                <>
-                  <Link passHref href={`/events/edit/${event.id}`}>
-                    <Button>Edit Event</Button>
+      {event && (
+        <div className="event-card-container">
+          <Card className="event-card" style={{ width: '600px', height: '800px' }}>
+            <Card.Header>{event.name}</Card.Header>
+            <div className="event-card-detail">
+              <Card.Img variant="top" className="event-card-img" src={event.image_url} style={{ width: '300px' }} />
+              <Card.Body>
+                <Card.Text>{event.description}</Card.Text>
+                <Card.Text>Ticket Price ${event.ticket?.price}</Card.Text>
+                <Card.Text>Tickets Available: {event.tickets_available}</Card.Text>
+                <Card.Text>{event.date && format(new Date(event.date), 'yyyy-M-d, h:mmbbb')}</Card.Text>
+                <Card.Text>
+                  Seller:{' '}
+                  <Link className="seller-link" passHref href={`/seller/${event.seller?.id}`}>
+                    {`${event.seller?.first_name} ${event.seller?.last_name}`}
                   </Link>
-                  <Button variant="danger" onClick={handleDelete}>
-                    Delete Event
+                </Card.Text>
+                {event.seller?.id !== user.id ? (
+                  <Button variant="primary" onClick={handleAddToCart}>
+                    Add ticket to Cart
                   </Button>
-                </>
-              )}
-            </Card.Body>
-          </div>
-        </Card>
-      </div>
+                ) : (
+                  <>
+                    <Link passHref href={`/events/edit/${event.id}`}>
+                      <Button>Edit Event</Button>
+                    </Link>
+                    <Button variant="danger" onClick={handleDelete}>
+                      Delete Event
+                    </Button>
+                  </>
+                )}
+              </Card.Body>
+            </div>
+          </Card>
+        </div>
+      )}
     </>
   );
 }
